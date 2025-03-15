3f46f9901b21c8acbda6d95d43bf049d33769ca6
sharath0404 <sharathfor@gmail.com>
2025-03-15 12:19:41 +0530

    ewew

---
+++ prac/condition.py
@@ -0,0 +1,21 @@
+#condition
+
+a = 10
+b = 20
+
+if a > b:
+    print(f" the value {a} is greater")
+elif b > a:
+    print(f"The value {b} is greater")
+else:
+    print("They are equal")
+    
+# while loop
+
+x = 0
+while x < 10:
+    print(x)
+    x = x + 1
+
+for i in range(10)
+    print (i)
